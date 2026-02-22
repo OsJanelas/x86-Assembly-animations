c12c25933c0fbb33365010a7d20ec2747516fc09
OsJanelas <osjanelas@gmail.com>
2026-02-22 12:41:39 -0300

    Add files via upload

---
+++ compilingimages.sh
@@ -0,0 +1,8 @@
+nasm -f bin art.asm -o Art.img
+nasm -f bin aurora.asm -o Aurora.img
+nasm -f bin formule.asm -o Formule.img
+nasm -f bin julia.asm -o Julia.img
+nasm -f bin sierspinki2.asm -o Sierspinki2.img
+nasm -f bin sierspinski.asm -o Sierspinski.img
+nasm -f bin static.asm -o Static.img
+nasm -f bin xor.asm -o Xor.img
