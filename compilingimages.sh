866f785b1455befec615e3870b18214636d3a0e5
OsJanelas <osjanelas@gmail.com>
2026-03-09 19:07:38 -0300

    Update compilingimages.sh

--- compilingimages.sh
+++ compilingimages.sh
@@ -1,8 +1,9 @@
-nasm -f bin art.asm -o Art.img
-nasm -f bin aurora.asm -o Aurora.img
-nasm -f bin formule.asm -o Formule.img
-nasm -f bin julia.asm -o Julia.img
-nasm -f bin sierspinki2.asm -o Sierspinki2.img
-nasm -f bin sierspinski.asm -o Sierspinski.img
-nasm -f bin static.asm -o Static.img
-nasm -f bin xor.asm -o Xor.img
+nasm -f bin Source/art.asm -o Art.img
+nasm -f bin Source/aurora.asm -o Aurora.img
+nasm -f bin Source/formule.asm -o Formule.img
+nasm -f bin Source/julia.asm -o Julia.img
+nasm -f bin Source/sierspinki2.asm -o Sierspinki2.img
+nasm -f bin Source/sierspinski.asm -o Sierspinski.img
+nasm -f bin Source/static.asm -o Static.img
+nasm -f bin Source/xor.asm -o Xor.img
+nasm -f bin Source/null.asm -o NullRoto.img
